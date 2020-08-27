a773189845f4510041a76310d79f6b0b174e5d98
Vijay <vijaysn02@gmail.com>
2020-08-27 08:29:58 +0530

    Initial Commit

---
+++ View Pager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  View Pager
+//
+//  Created by Sagaya Navis Vijay on 27/08/20.
+//  Copyright © 2020 vijay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
